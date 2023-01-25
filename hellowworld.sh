5358c82443c08d098965bade662e79cfec2900d0
N3wBr0dy <122485846+N3wBr0dy@users.noreply.github.com>
2023-01-25 02:58:13 +0000

    Delete hellowworld.sh

--- hellowworld.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Author: Joseph Smith - student# 200340294
-#Date: January 24th, 2023
-
-#this displays "hello world"
-echo Hello World!
-
-#this ends the script (not necessary, but sure why not)
-exit
