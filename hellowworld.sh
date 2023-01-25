056fed899abfda225d6ec7ff28ef82d9c5c9d1ac
N3wBr0dy <122485846+N3wBr0dy@users.noreply.github.com>
2023-01-25 02:57:05 +0000

    Create hellowworld.sh

---
+++ hellowworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Author: Joseph Smith - student# 200340294
+#Date: January 24th, 2023
+
+#this displays "hello world"
+echo Hello World!
+
+#this ends the script (not necessary, but sure why not)
+exit
