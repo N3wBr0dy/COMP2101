142422c381b8176dcabdb02a13a5def70e6d1aa4
N3wBr0dy <122485846+N3wBr0dy@users.noreply.github.com>
2023-04-11 16:04:19 -0400

    Create systemreport.ps1

---
+++ Powershell/systemreport.ps1
@@ -0,0 +1,40 @@
+﻿############################
+##COMP2101 - Lab 5##########
+##Joseph Smith - 200340294##
+############################
+
+Param ([switch]$SystemReport, [switch]$DisksReport,
+       [switch]$NetworkReport)
+
+Import-Module Module200340294
+
+if ($SystemReport -eq $false -and $DisksReport -eq $false -and $NetworkReport -eq $false) {
+"------------------------
+System Report:
+------------------------"
+      get-systemReport
+      "
+      "
+      get-diskReport
+      "
+      "
+      get-netReport
+} 
+    else {
+
+      if ($System) {
+        get-systemReport
+        }
+
+      if ($Disks) {
+        get-diskReport
+        }
+
+      if ($Network) {
+        get-netReport
+        }
+}
+
+#################
+##END OF SCRIPT##
+#################
